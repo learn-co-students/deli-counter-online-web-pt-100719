bc337afc6063c0e1617c0802d4fba050ee571c8a
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-10-14 20:15:45 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+
+def line(customers)
+  
+string = "The line is currently"
+
+  if customers.length < 1 
+    string << " empty."
+  else
+    string << ":"
+  end
+  customers.each_with_index do |customer, index|
+    new_index = index + 1
+    string << " #{new_index}. #{customer}"
+  end
+  puts string
+end
+
+def take_a_number(array,string)
+
+  array << string
+  spot = array.index(string) + 1
+  puts "Welcome, #{string}. You are number #{spot} in line."
+end
+
+def now_serving(array)
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+  else 
+    current_customer = array.shift
+    puts "Currently serving #{current_customer}."
+  end
+end
