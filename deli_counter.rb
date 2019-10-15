e0840165499b1e16481ed3b2783dc5db15b2acaf
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-15 23:41:58 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,18 +1,29 @@
 require 'pry'
 
-katz_deli = []
-
-def line(customers)
+def line(katz_deli)
   people = 1
   the_line = ""
-  if customers.length < 1
+  if katz_deli.length < 1
     puts "The line is currently empty."
   else
     the_line << "The line is currently:"
-    customers.each do |c|
+    katz_deli.each do |c|
     the_line << " #{people}. #{c}" 
     people += 1
     end
     puts the_line
+  end
+end
+
+def take_a_number(katz_deli,name)
+  order = ""
+  place = 1
+if name != nil
+  katz_deli << name
+  katz_deli.each do |n|
+  order << "Welcome, #{n}. You are number #{place} in line."
+  place += 1
+    end
+    puts order
   end
 end
