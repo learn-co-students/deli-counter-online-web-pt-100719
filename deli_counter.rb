981a8830914e8c138fb6d94b55c8c13bf6dadc2d
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-15 09:37:53 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,9 @@
 require 'pry'
-def line(people)
-    binding.pry
+def line(customers)
+  people = 0
+  if customers.length < 1
+    puts "The line is currently empty."
+  else
+    people += 1
+  end
 end
