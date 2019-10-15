ab887f486f5c744944ae66f61000528ff5493d58
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-15 09:00:24 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,21 +1,31 @@
 katz_deli = []
  
-  take_a_number(katz_deli, "Ada")
-  
-  take_a_number(katz_deli, "Grace")
-  
-  take_a_number(katz_deli, "Kent") 
- 
-  line(katz_deli) 
- 
-  now_serving(katz_deli)
- 
-  line(katz_deli) 
- 
-  take_a_number(katz_deli, "Matz")
- 
-  line(katz_deli) 
- 
-  now_serving(katz_deli) 
- 
-  line(katz_deli) #=>
+def take_a_number(katz_deli, "Ada")
+end
+
+def take_a_number(katz_deli, "Grace")
+end
+
+def take_a_number(katz_deli, "Kent") 
+end
+
+def line(katz_deli) 
+end
+
+def now_serving(katz_deli)
+end
+
+def line(katz_deli) 
+end
+
+def take_a_number(katz_deli, "Matz")
+end
+
+def  line(katz_deli) 
+end
+
+def now_serving(katz_deli) 
+end
+
+def line(katz_deli) 
+end
