640e61c37797d29177ab8608b51623daeb477861
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-15 09:22:54 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,31 +1,4 @@
-katz_deli = []
- 
-def take_a_number(katz_deli, "Ada")
-end
-
-def take_a_number(katz_deli, "Grace")
-end
-
-def take_a_number(katz_deli, "Kent") 
-end
-
-def line(katz_deli) 
-end
-
-def now_serving(katz_deli)
-end
-
-def line(katz_deli) 
-end
-
-def take_a_number(katz_deli, "Matz")
-end
-
-def  line(katz_deli) 
-end
-
-def now_serving(katz_deli) 
-end
-
-def line(katz_deli) 
+require 'pry'
+def line(people)
+    binding.pry
 end
