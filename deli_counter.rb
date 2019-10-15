da3a21fa5d1bfe0bd36759e2143bac799605bd0e
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-15 22:20:33 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,17 @@
 require 'pry'
 def line(customers)
-  people = 0
+  people = 1
+  the_line = ""
   if customers.length < 1
     puts "The line is currently empty."
   else
+    the_line << "The line is currently:"
+    customers.each do |c|
+    the_line << " #{people}. #{c}" 
     people += 1
+    end
+    puts the_line
   end
 end
+
+def take_a_number()
