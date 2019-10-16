49ea9e25252889a5ce66dffae4ca30f684978903
Artiium Arbyummi <artiium21@gmail.com>
2019-10-16 03:14:55 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def line(array)
+  if array.empty?
+    puts "The line is currently empty."
+  else 
+    current_line =  "The line is currently:"
+   array.each_with_index(1) do |person, index| 
+     #{index}. #{person}"
+  end
+end
+
+ 
