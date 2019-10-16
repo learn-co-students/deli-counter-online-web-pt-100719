b87c750b17dc81839452827a11b07151e24be226
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-16 03:18:52 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,15 +16,16 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli,name)
-  last = ""
-  order = ""
-  place = 1
-if name != nil 
+place = 1
+  if katz_deli.length < 1
   katz_deli << name
-  katz_deli.each do |n|
-  order << "Welcome, #{n}. You are number #{place} in line."
+  puts "Welcome, #{name}. You are number #{place} in line."
   place += 1
-    end
-    puts order
+  elsif
+  katz_deli.length > 1
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+else
+  katz_deli.join << name.split
   end
 end
