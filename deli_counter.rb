ff67322adb50cc6f94bae585657ecd18c12b8eef
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-16 08:48:01 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,6 +26,15 @@ place = 1
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 else
-  katz_deli.join << name.split
+  katz_deli << name
+  end
+end
+
+def now_serving(katz_deli)
+if katz_deli.length == 0
+  puts "There is nobody waiting to be served!"
+else
+  puts "Currently serving #{katz_deli[0]}."
+  katz_deli.shift
   end
 end
