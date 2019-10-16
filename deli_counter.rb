cbe935ba9c768a7ec53d4eb19c898fdd97115b14
Derric Fragosa <fderric@gmail.com>
2019-10-16 03:48:43 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+
+katz_deli =[]
+def line (array)
+  if array.length == 0 
+    puts"The line is currently empty."
+    
+  end 
+end 
+
+def take_a_number(array,index)
+  # start index @ 1. 
+end 
+
+def now_serving 
+end 
