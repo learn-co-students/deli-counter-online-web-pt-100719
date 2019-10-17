bde65e5a268f5d44628212861babc58209e60b98
Derric Fragosa <fderric@gmail.com>
2019-10-17 21:38:09 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line (array)
     if array.length == 0 
     puts "The line is currently empty."
   else
-     queue = "The line is currently:"
+     
     array.map.with_index(1) do |name, place| 
     "The line is currently: #{place}. #{name}"
    end 
