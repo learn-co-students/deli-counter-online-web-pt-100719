b5ab77b0567ff06de686f74b44a59ac192894e2f
Derric Fragosa <fderric@gmail.com>
2019-10-18 04:57:42 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,23 +3,26 @@ katz_deli = ["Andrew","Sammy","Jalysia"]
 
 def line(array)
   if array.length >= 1
-    queue = []
-    counter = 1 
+  queue = []
+  counter = 1 
     array.each do |name|
     queue.push("#{counter}. #{name}")
     counter += 1 
     end  
     puts "The line is currently: #{queue.join(" ")}"
-  else
+    else
     puts "The line is currently empty."
   end 
 end 
 line(katz_deli)
 
 
-def take_a_number(array,index)
-  # strt index @ 1. 
+def take_a_number(line,customer)
+  line << customer
+  puts "Welcome, #{customer}. You are number #{line.length} in line."
+  #no one in line/add them to array/line 
 end 
+take_a_number(katz_deli,"Derric")
 
-def now_serving 
-end 
+# def now_serving 
+# end 
