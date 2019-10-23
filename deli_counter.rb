b54928eb46917d16fa5e74c5da3701693dad9243
Phil Billingsby <philbillingsby@gmail.com>
2019-10-23 18:25:25 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,28 +3,26 @@ katz_deli = []
 
 
 def line(array)
-  if array.length == 0
+  if array.length == 0 
     puts "The line is currently empty."
   else
-    message = "The line is currently:" 
-  array.each_with_index do |value, index|
-    message += " #{index.to_i + 1}. #{value}" 
-  end
-    puts "#{message}"
+    line_msg = "The line is currently:"
+    array.each_with_index {|value, index| line_msg += " #{index.to_i + 1}. #{value}"}
+    puts "#{line_msg}"
   end
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli << name # adds name to katz_deli array
+  katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
 end
 
-def now_serving(n)
-  if n.count == 0
+def now_serving(people)
+  if people.count == 0
     puts "There is nobody waiting to be served!"
   else
-    name = n[0] # first name in line
-    puts "Currently serving #{name}." 
-    n.shift
+    name = people[0]
+    puts "Currently serving #{name}."
+    people.shift
   end
 end
